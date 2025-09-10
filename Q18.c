0f69d689ba8baebca98b4aaa0739aad11aa74dfc
Gunjanchoprax <gunjanchopra213@gmail.com>
2025-09-10 23:28:09 +0530

    Create Q18.c

---
+++ Q18.c
@@ -0,0 +1,23 @@
+// Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria:
+#include<stdio.h>
+int main(){
+    int percentage;
+    printf("Enter your percentage:");
+    scanf("%d",&percentage);
+
+        if(percentage>=90 && percentage <=100){
+            printf("Grade A");}
+                   else if(percentage>=80 && percentage <=89){
+                           printf("Grade B");}
+                                 else if(  percentage>=70 && percentage <=79){
+                                         printf("Grade C");}
+                                                else if(percentage>=60 && percentage<=69){
+                                                        printf("Grade D");}
+                                                               else if(percentage<=60){
+                                                                    printf("Grade F");}
+                                                                          else {
+                                                                            printf("Invalid percentage");}
+                         
+         
+       return 0;  
+}
