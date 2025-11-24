0ef99ac8fee83acfb9db2e12ff7c35822b767f31
Gunjanchoprax <gunjanchopra213@gmail.com>
2025-11-24 11:41:50 +0530

    Create Q74.c

---
+++ Q74.c
@@ -0,0 +1,43 @@
+//Find the transpose of a matrix
+#include<stdio.h>
+int main(){
+
+    int a[3][3];  
+    int i, j;
+    int row , col;
+
+    printf("Enter here:\n");
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%d\t", a[i][j]);
+            
+        }
+        printf("\n");
+    }
+    printf("Transpose of Matrix is\n");
+ for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%d\t", a[j][i]);
+            
+        }
+        printf("\n");
+    }
+
+  
+
+   
+    return 0;
+}
