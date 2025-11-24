d603aeeace3160c3117c846442001330eb838f1e
Gunjanchoprax <gunjanchopra213@gmail.com>
2025-11-24 23:09:09 +0530

    Improve name initials and surname display logic
    Refactor name processing to print initials and surname.

--- Q98.c
+++ Q98.c
@@ -1,34 +1,24 @@
-//Print initials of a name with the surname displayed in full.
+/*Print initials of a name with the surname displayed in full.*/
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main() {
     char name[100];
-    printf("Enter your full name: ");
-    gets(name);  
-
+    printf("Enter full name: ");
+    gets(name);
     int len = strlen(name);
-    int lastSpace = -1;
-
-    // Find position of last space (to get surname)
-                  for (int i = 0; i < len; i++) {
-        if (name[i] == ' ')
-            lastSpace = i;
-    }
-
-    // Print initials of all names except surname
-    printf("Result: ");
-    for (int i = 0; i < len; i++) {
-        if ((i == 0 || name[i - 1] == ' ') && i < lastSpace) {
-            printf("%c.", toupper(name[i]));
+    int i = 0;
+    while (i < len) {
+        while (name[i] == ' ') i++;
+        int start = i;
+        while (i < len && name[i] != ' ') i++;
+        if (i < len) {
+            printf("%c. ", name[start]);
+        }
+         else
+         {
+            printf("%s", &name[start]);
         }
     }
-
-    // Print surname in full
-    for (int i = lastSpace + 1; i < len; i++) {
-        printf("%c", name[i]);
-    }
-
     return 0;
 }
